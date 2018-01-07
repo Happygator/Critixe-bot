f56fb4825791784c34a74fc90613bf7f9522a08f
Happygator <perng.arthur@gmail.com>
2018-01-06 18:17:47 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
   if (message.content === '!help') {
     message.reply('Happygator4723 is currently working on this bot. It has no usable features yet. #blamecritixe')
   }
+  if (message.content === '!critixe') {
+    message.reply('<@398308436279820288> <@398308436279820288> <@398308436279820288> <@398308436279820288> <@398308436279820288>')
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
