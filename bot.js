81790a7cc6a29e8c170e7463d0c3b7b2835f9296
Happygator <perng.arthur@gmail.com>
2018-01-09 16:29:49 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var roll1,roll2,roll3,roll4,roll5
-
 client.on('ready', () => {
   console.log('I am ready!');
 });
@@ -40,11 +39,11 @@ client.on('message', message => {
     
   }
   if (message.content === '!rollYes') {
-    message.reply("You roll " + String(Math.floor(Math.random() * 1000) + 1))
-  }
-  if (message.content === '!rollOof') {
-    message.reply("You roll " + String(Math.floor(Math.random() * 9001) + 1000))
-  }
+    if (message.member.roles.some(r=>["Server Owner", "MODERATOR"].includes(r.name)) ) {
+      message.reply("You roll " + String(Math.floor(Math.random() * 9001) + 1000))
+    } else {
+      message.reply("You roll 0 )
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
