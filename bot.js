3f5e297a68f626becbef57c135197a62b33aa383
Happygator <perng.arthur@gmail.com>
2018-01-09 16:34:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,8 +42,8 @@ client.on('message', message => {
     if (message.author.username === "Happygator4723") {
       message.reply("You roll " + String(Math.floor(Math.random() * 9001) + 1000))
     } else {
-      message.reply("You roll 0")
-    }
+    message.reply("You roll 0")  
+}
 });
 
 client.login(process.env.BOT_TOKEN);
